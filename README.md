41067b96b11850acaf86895ab61196235df90a87
exebeeche <44062246+exebeeche@users.noreply.github.com>
2023-03-28 13:10:53 +0300

    Adding projects

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # C-sharp-projects
-My study projects
+
+This repo includes my study projects in C#.
+
+In this list, you can find the console apps that I have done in C#:
+
+* [Simple apps]()
+* [Advanced apps]()
+
+In this list, there are the first GUI apps that I have done in C#:
+
+* [Employee class]()
+
---
+++ VolumeCompare/CheckCapitalLetter/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CheckCapitalLetter {
+    internal class Program {
+        static void Main(string[] args) {
+
+            // Input a symbol
+            do {
+                Console.WriteLine("Enter a letter:");
+                char checkedLetter = Console.ReadKey().KeyChar;
+                Console.ReadLine();
+
+                // Check the letter
+
+                //char.IsUpper(checkedLetter) ? Console.WriteLine("The input symbol is an uppercase letter.") : Console.WriteLine("The input symbol is a capital letter.");
+
+                if(char.IsUpper(checkedLetter)) {
+                    Console.WriteLine("The input symbol is an uppercase letter.");
+                } else {
+                    Console.WriteLine("The input symbol is a capital letter.");
+                }
+            } while (true);
+            }
+    }
+}
